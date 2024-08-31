fa3e9880e37d947843bf2b24d66792c31abea46a
Supriya Singh <124588951+Supriya334@users.noreply.github.com>
2024-08-31 19:27:21 +0530

    Update script.js

--- MovieApp/script.js
+++ MovieApp/script.js
@@ -8,7 +8,7 @@ let getMovie = () => {
   let movieName = movieNameRef.value;
   let url = `http://www.omdbapi.com/?t=${movieName}&apikey=${key}`;
   //If input field is empty
-  if (movieName.length <= 0) {
+  if (movieName.length <= 0) {     
     result.innerHTML = `<h3 class="msg">Please Enter A Movie Name</h3>`;
   }
   //If input field is NOT empty
