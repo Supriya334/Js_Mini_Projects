e0c2995365a5f94d027795fc98c7d2bfc1f4bb76
Supriya Singh <124588951+Supriya334@users.noreply.github.com>
2024-09-04 21:07:09 +0530

    Update script.js

--- TheRecipeApp/script.js
+++ TheRecipeApp/script.js
@@ -6,7 +6,7 @@ let url = "https://www.themealdb.com/api/json/v1/1/search.php?s="
 
 searchBtn.addEventListener("click", () => {
   let userInp = document.getElementById("user-inp").value;
-  if (userInp.length == 0) {
+  if (userInp.length == 0) {   
     result.innerHTML = `<h3>Input Field Cannot Be Empty</h3>`;
   } else {
     fetch(url + userInp)
