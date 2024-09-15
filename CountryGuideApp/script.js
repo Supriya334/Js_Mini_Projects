1ac5569d3e4b68107a3e96a152494d1f80aa7193
Supriya Singh <124588951+Supriya334@users.noreply.github.com>
2024-09-15 21:49:28 +0530

    Update script.js

--- CountryGuideApp/script.js
+++ CountryGuideApp/script.js
@@ -3,7 +3,7 @@ let countryInp = document.getElementById("country-inp");
 
 searchBtn.addEventListener("click", () => {
     let countryName = countryInp.value;
-    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
+    let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;   
 
     console.log(finalURL);
   
